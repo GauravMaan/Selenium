c1461e08afc7cef3bcddef8efdd8f6f8a5f32216
gauravmaan <gauravmaan0009@gmail.com>
2024-12-17 14:03:18 +0530

    Final commit

---
+++ PracticeSession1/Link_Text.py
@@ -0,0 +1,16 @@
+import time
+from re import search
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Safari()
+driver.get("https://www.selenium.dev/documentation/")
+
+search_link=driver.find_element(By.PARTIAL_LINK_TEXT,"Web")
+search_link.click()
+time.sleep(3)
+search_link1=driver.find_element(By.LINK_TEXT,"Actions API")
+search_link1.click()
+time.sleep(2)
+driver.quit()
+print("Browser closed.")
